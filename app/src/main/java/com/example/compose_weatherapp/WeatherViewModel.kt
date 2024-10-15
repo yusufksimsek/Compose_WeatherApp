ece653e0b4a6f64cf2789febea5c2c76f5892839
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-10-15 15:42:52 +0300

    Network Response class created

--- app/src/main/java/com/example/compose_weatherapp/WeatherViewModel.kt
+++ app/src/main/java/com/example/compose_weatherapp/WeatherViewModel.kt
@@ -1,22 +1,37 @@
 package com.example.compose_weatherapp
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.compose_weatherapp.api.Constant
+import com.example.compose_weatherapp.api.NetworkResponse
 import com.example.compose_weatherapp.api.RetrofitInstance
+import com.example.compose_weatherapp.api.WeatherModel
 import kotlinx.coroutines.launch
 
 class WeatherViewModel : ViewModel() {
 
     private val weatherApi = RetrofitInstance.weatherApi
+    private val _weatherResult = MutableLiveData<NetworkResponse<WeatherModel>>()
+    private val weatherResult : LiveData<NetworkResponse<WeatherModel>> = _weatherResult
     fun getData(city: String){
+        _weatherResult.value = NetworkResponse.Loading
         viewModelScope.launch {
-            val response = weatherApi.getWeather(Constant.apiKey,city)
-            if(response.isSuccessful){
-                Log.i("Response: ", response.body().toString())
-            }else {
-                Log.i("Error: ", response.message())
+            try {
+                val response = weatherApi.getWeather(Constant.apiKey,city)
+                if(response.isSuccessful){
+                    response.body()?.let {
+                        _weatherResult.value = NetworkResponse.Success(it)
+                    }
+                }else {
+                    _weatherResult.value = NetworkResponse.Error("Failed to load data")
+                }
+            }
+            catch (e: Exception){
+                _weatherResult.value = NetworkResponse.Error("Failed to load data")
+
             }
         }
     }
