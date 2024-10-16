7eba06dacd9e9f9359d88ec134bae2fb6fd13fa0
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-10-16 15:42:04 +0300

    Added location icon, city and country name within WeatherDetailScreen

--- app/src/main/java/com/example/compose_weatherapp/WeatherPage.kt
+++ app/src/main/java/com/example/compose_weatherapp/WeatherPage.kt
@@ -3,9 +3,13 @@ package com.example.compose_weatherapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
@@ -20,8 +24,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compose_weatherapp.api.NetworkResponse
+import com.example.compose_weatherapp.api.WeatherModel
 
 @Composable
 fun WeatherPage(viewModel: WeatherViewModel){
@@ -69,10 +76,35 @@ fun WeatherPage(viewModel: WeatherViewModel){
                 CircularProgressIndicator()
             }
             is NetworkResponse.Success -> {
-                Text(text = result.data.toString())
+                WeatherDetails(data = result.data)
             }
             null -> {}
         }
 
+    }
+}
+
+@Composable
+fun WeatherDetails(data: WeatherModel){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = "Location icon",
+                modifier = Modifier.size(40.dp)
+                )
+            Text(text = data.location.name, fontSize = 30.sp)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = data.location.country, fontSize = 18.sp, color = Color.Gray)
+        }
     }
 }
